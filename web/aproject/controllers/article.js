de8aff69eb4b72087e3bd3b5a135467116490902
Jānis Gruzis <jgruzis@gmail.com>
2015-04-16 23:26:11 +0300

    Images from real source

--- web/aproject/controllers/article.js
+++ web/aproject/controllers/article.js
@@ -1,113 +1,70 @@
-controllers.controller('ArticleController', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
-        $scope.news = [];
-        for (var i = 0; i < 10; i++) {
-            $scope.news.push([{id: 0,
-                    type: 'type',
-                    category_id: 0,
-                    title: 'Latvijas hokeja izlases sastāvā PČ nespēlēs arī Bārtulis',
-                    description: 'Čitogaras (Chittorgarh) cietoksnis ir lielākais forts Indijā. Tā ir fascinējoša vieta, jo atgādina milzīgu klinšu salu, ko var redzēt pat no lidmašīnas, raksta slavenais ceļvedis Lonely Planet. Sešu kilometru garumā cietoksnis slēpjas starp 150 līdz 18 metrus augstām klintīm.',
-                    image: 'http://placehold.it/350x350',
-                    cluster_id: 0,
-                    rating: 0,
-                    url_mobile: 'http://4chan.org/b/',
-                    url: 'https://www.google.lv',
-                    source: 'delfi.lv',
-                    date: "45 minutes ago" //"4/12/2015, 3:34:51 AM"
-                //tokens: 
-                }, 
-                {id: 0,
-                    type: 'type',
-                    category_id: 0,
-                    title: 'Latvijas hokeja izlases sastāvā PČ nespēlēs arī Bārtulis',
-                    description: 'Čitogaras (Chittorgarh) cietoksnis ir lielākais forts Indijā. Tā ir fascinējoša vieta, jo atgādina milzīgu klinšu salu, ko var redzēt pat no lidmašīnas, raksta slavenais ceļvedis Lonely Planet. Sešu kilometru garumā cietoksnis slēpjas starp 150 līdz 18 metrus augstām klintīm.',
-                    image: 'http://placehold.it/350x350',
-                    cluster_id: 0,
-                    rating: 0,
-                    url_mobile: 'http://4chan.org/b/',
-                    url: 'https://www.google.lv',
-                    source: 'delfi.lv',
-                    date: moment("4/12/2015, 3:34:51 AM").fromNow()
-                //tokens: 
-                }, 
-                {id: 0,
-                    type: 'type',
-                    category_id: 0,
-                    title: 'Latvijas hokeja izlases sastāvā PČ nespēlēs arī Bārtulis',
-                    description: 'Čitogaras (Chittorgarh) cietoksnis ir lielākais forts Indijā. Tā ir fascinējoša vieta, jo atgādina milzīgu klinšu salu, ko var redzēt pat no lidmašīnas, raksta slavenais ceļvedis Lonely Planet. Sešu kilometru garumā cietoksnis slēpjas starp 150 līdz 18 metrus augstām klintīm.',
-                    image: 'http://placehold.it/350x350',
-                    cluster_id: 0,
-                    rating: 0,
-                    url_mobile: 'http://4chan.org/b/',
-                    url: 'https://www.google.lv',
-                    source: 'delfi.lv',
-                    date: "45 minutes ago"
-                //tokens: 
-                }]);
-        }
-        
-        $scope.isMobile = function() {
-            var check = false;
-            (function(a) {
-                if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
-                    check = true
-            })(navigator.userAgent || navigator.vendor || window.opera);
-            return check;
-        };
-        
-        $scope.getUrl = function(subItem) {
-            return ($scope.isMobile() ? subItem.url_mobile : subItem.url);
-        };
-        
-        $scope.getMoment = function(date) {
-            return moment(date).fromNow();
-        };
-        
-        $scope.showSubitems = function(index) {
-            angular.element($('#subItems-' + index)).toggle();
-        };
+controllers.controller('ArticleController', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
 
-        /**
+	$scope.isMobile = function () {
+		var check = false;
+		(function (a) {
+			if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
+				check = true
+		})(navigator.userAgent || navigator.vendor || window.opera);
+		return check;
+	};
+
+	$scope.getUrl = function (subItem) {
+		return ($scope.isMobile() ? subItem.url_mobile : subItem.url);
+	};
+
+	$scope.getMoment = function (date) {
+		return moment(date).fromNow();
+	};
+
+	$scope.showSubitems = function (index) {
+		angular.element($('#subItems-' + index)).toggle();
+	};
+
+	/**
 	 * Load cluster articles.
 	 */
-        var loadArticles = function() {
-            
-            var clusterIds = _.map($scope.clusters, function(cluster) {
-                return cluster.id;
-            });
-            
-            var query = $.param({'clusters': clusterIds});
-            var url = 'http://api.deino.clevercode.lv/api/articles_of_clusters?' + query;
-            
-            $http.get(url)
-            .success(function(data) {
-                $.each(data, function(i, val) {
-                    $.each(val, function(j, valSub) {
-                        valSub.image = "http://g4.delphi.lv/images/pix/520x315/Bx4b2w06dZE/indrasis-44273807.jpg";
-                    });
-                })
-                $scope.articles = data;
-            });
-        };
+	var loadArticles = function () {
 
-        /**
+		var clusterIds = _.map($scope.clusters, function (cluster) {
+			return cluster.id;
+		});
+
+		var query = $.param({'clusters': clusterIds});
+		var url = 'http://api.deino.clevercode.lv/api/articles_of_clusters?' + query;
+
+		$http.get(url)
+			.success(function (data) {
+				/**
+				$.each(data, function (i, val) {
+					$.each(val, function (j, valSub) {
+						valSub.image = "http://g4.delphi.lv/images/pix/520x315/Bx4b2w06dZE/indrasis-44273807.jpg";
+					});
+				})
+				 */
+				$scope.articles = data;
+			});
+	};
+
+	/**
 	 * Load categories.
 	 */
-        $http.get('http://api.deino.clevercode.lv/api/categories')
-        .success(function(data) {
-            $scope.categories = data;
-        });
+	$http.get('http://api.deino.clevercode.lv/api/categories')
+		.success(function (data) {
+			$scope.categories = data;
+		});
 
-        /**
+	/**
 	 * Load clusters.
 	 */
-        $http.get('http://api.deino.clevercode.lv/api/grouped_clusters')
-        .success(function(data) {
-            $scope.clusters = _.flatten(_.toArray(data), true);
-            loadArticles();
-        });
+	$http.get('http://api.deino.clevercode.lv/api/grouped_clusters')
+		.success(function (data) {
+			$scope.clusters = _.flatten(_.toArray(data), true);
+			loadArticles();
+		});
 // //$scope.$on('$viewContentLoaded', function(){
 // $scope.$on('$viewContentLoaded', function(event) {
-//  // Run after view loaded.	
+//  // Run after view loaded.
 // 	var defaults      = {
 // 	  selector:             '[data-adaptive-background="1"]',
 // 	  parent:               null,
@@ -122,6 +79,6 @@ controllers.controller('ArticleController', ['$rootScope', '$scope', '$http', fu
 // 		dark:       "ab-dark"
 // 	  }
 // 	};
-// 	$.adaptiveBackground.run(defaults);        
+// 	$.adaptiveBackground.run(defaults);
 // });
-    }])
+}])
