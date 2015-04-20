fff42e07dd7dfb97d554c79a25062c32ae19d938
Inwhite <aleksandrs.rilins@gmail.com>
2015-04-20 20:40:19 +0300

    proper popular syntax

--- web/aproject/controllers/article.js
+++ web/aproject/controllers/article.js
@@ -1,16 +1,27 @@
 controllers.controller('ArticleController', ['$rootScope', '$scope', '$http',
-	function ($rootScope, $scope, $http) {
-		/**
-		 * Load categorized cluster articles.
-		 */
-		var url = 'http://api.deino.clevercode.lv/api/articles_by_category';
-		$http.get(url)
-			.success(function (data) {
-				//Dirtiest hack for tests
-				var stub_data= data;
-				stub_data[0] = data.local;
-				$rootScope.categories[0] = {name:'Populārākas'};
-				$scope.catClustArticles = stub_data;
-			});
-	}]);
+    function ($rootScope, $scope, $http) {
+        /**
+         * Load categorized cluster articles.
+         */
+        var url = 'http://api.deino.clevercode.lv/api/articles_by_category';
+        $http.get(url)
+            .success(function (data) {
+                $rootScope.categories[0] = {name: 'Populārākas'};
+                $scope.catClustArticles = data;
+                $rootScope.categories[0] = {name: 'Populārākas'};
+                var popular_url = 'http://api.deino.clevercode.lv/api/popular';
+                $http.get(popular_url)
+                    .success(function (popular_raw) {
+                        var popular_articles = [];
+                        for (var i in popular_raw) {
+                            popular_articles = popular_articles.concat(popular_raw[i]);
+                        }
+                        var articles = $scope.catClustArticles;
+                        articles[0] = popular_articles;
+                        console.log(popular_articles);
+                        $scope.catClustArticles = articles;
+                    });
+            });
+
+    }]);
 
