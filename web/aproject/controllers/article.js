a33077d29eb67bfd79ee3dfe1d7ed5a21a2afd0c
Inwhite <aleksandrs.rilins@gmail.com>
2015-04-20 13:19:59 +0300

    IMG stub for empty img

--- web/aproject/controllers/article.js
+++ web/aproject/controllers/article.js
@@ -6,7 +6,7 @@ controllers.controller('ArticleController', ['$rootScope', '$scope', '$http',
 		var url = 'http://api.deino.clevercode.lv/api/articles_by_category';
 		$http.get(url)
 			.success(function (data) {
-				//Dirties hack for tests
+				//Dirtiest hack for tests
 				var stub_data= data;
 				stub_data[0] = data.local;
 				$rootScope.categories[0] = {name:'Populārākas'};
